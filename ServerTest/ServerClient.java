b3a3a8698f82a1779cbfeb73c516660297cab305
Eric Rylander <Rylle98@hotmail.se>
2019-02-11 14:28:06 +0100

    Första implementation av en server

---
+++ ServerTest/ServerClient.java
@@ -0,0 +1,95 @@
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerClient {
+    public static void main (String[] arg)
+    {
+	ServerSocket ss = null;
+	Socket s = null;
+	System.out.println("Server will run until user type quit");
+	try{
+	     ss = new ServerSocket(9999);
+	}catch (Exception e)
+	    {
+		System.out.println(e);
+	    }
+	CommandlineScanner Scan = new CommandlineScanner("Scanner");
+	Scan.start();
+	while(true)
+	    {
+		try{
+		    s = ss.accept();
+		    Server st = new Server(s);
+		    st.start();
+		}
+		catch(Exception e)
+		    {
+			System.out.println(e);
+		    }
+	    }
+    }
+}
+
+
+class Server extends Thread{
+    Socket s;
+
+    public Server(Socket s)
+    {
+	this.s = s;
+    }
+    public void  run()
+    {
+	System.out.println("ServerSocket created");
+       	try
+	    {
+		DataInputStream textin = new DataInputStream(s.getInputStream());
+		DataOutputStream response = new DataOutputStream(s.getOutputStream()); 
+		while(true)
+		    {
+			String input = (String)textin.readUTF();
+			System.out.println(input);
+			response.writeUTF("response");
+			response.flush();
+			
+		    }       
+       	    }
+       	catch (Exception e)
+       	    {
+       		System.out.println(e);
+	    }
+    }
+}
+
+class CommandlineScanner implements Runnable{
+    private Thread t;
+    private String name;
+
+    public CommandlineScanner(String name)
+    {
+	this.name = name;
+    }
+			     
+    public void run()
+    {
+	System.out.println("CommandlineScanner created");
+	Scanner scan = new Scanner(System.in);
+	if(scan.nextLine().equals("quit"))
+	   {
+	       System.exit(0);
+	   }
+    }
+	public void start()
+    {
+	if(t==null)
+	    {
+		System.out.println("CommandlineScanner now starting");
+		t = new Thread(this,name);
+		t.start();
+	    }
+    }
+}
